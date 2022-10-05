5136fbed0f6ad51213f13d60a71cef9abbfcddc7
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2022-10-05 22:27:55 +0200

    Update index.js

--- back/index.js
+++ back/index.js
@@ -6,7 +6,7 @@ const port = 3000
 
 //Database
 const mongoose = require("mongoose")
-const password = "Nomade75"
+const password = ""
 const uri = `mongodb+srv://yanes:${password}@cluster0.fqyz4.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(uri).then((() => console.log("connected to Mongo"))).catch(err => console.error("Error connecting to Mongo: ", err))
 

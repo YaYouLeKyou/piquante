0e55d9692202bb33e7478a1d1f73f6e0d78c410e
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2022-10-18 18:45:27 +0200

    Refacto(models):added Sauce.js, User.js

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -1,29 +1,8 @@
-const mongoose = require("mongoose")
+const Product = require('../models/sauce')
 const {
   unlink
 } = require("fs/promises")
-const {
-  likeSauce
-} = require("./vote")
 
-const productSchema = new mongoose.Schema({
-  userId: String,
-  name: String,
-  manufacturer: String,
-  description: String,
-  mainPepper: String,
-  imageUrl: String,
-  heat: {
-    type: Number,
-    min: 1,
-    max: 5
-  },
-  likes: Number,
-  dislikes: Number,
-  usersLiked: [String],
-  usersDisliked: [String]
-})
-const Product = mongoose.model("Product", productSchema)
 
 function getSauces(req, res) {
   Product.find({})
@@ -145,6 +124,62 @@ function createSauce(req, res) {
     .catch((err) => res.status(500).send(err))
 }
 
+function likeSauce(req, res) {
+  const {
+    like,
+    userId
+  } = req.body
+  if (![1, -1, 0].includes(like)) return res.status(403).send({
+    message: "Invalid like value"
+  })
+
+  getSauce(req, res)
+    .then((product) => updateVote(product, like, userId, res))
+    .then((pr) => pr.save())
+    .then((prod) => sendClientResponse(prod, res))
+    .catch((err) => res.status(500).send(err))
+}
+
+function updateVote(product, like, userId, res) {
+  if (like === 1 || like === -1) return incrementVote(product, userId, like)
+  return resetVote(product, userId, res)
+}
+
+function resetVote(product, userId, res) {
+  const {
+    usersLiked,
+    usersDisliked
+  } = product
+  if ([usersLiked, usersDisliked].every((arr) => arr.includes(userId)))
+    return Promise.reject("User seems to have voted both ways")
+
+  if (![usersLiked, usersDisliked].some((arr) => arr.includes(userId)))
+    return Promise.reject("User seems to not have voted")
+
+  if (usersLiked.includes(userId)) {
+    --product.likes
+    product.usersLiked = product.usersLiked.filter((id) => id !== userId)
+  } else {
+    --product.dislikes
+    product.usersDisliked = product.usersDisliked.filter((id) => id !== userId)
+  }
+
+  return product
+}
+
+function incrementVote(product, userId, like) {
+  const {
+    usersLiked,
+    usersDisliked
+  } = product
+
+  const votersArray = like === 1 ? usersLiked : usersDisliked
+  if (votersArray.includes(userId)) return product
+  votersArray.push(userId)
+
+  like === 1 ? ++product.likes : ++product.dislikes
+  return product
+}
 
 
 module.exports = {
--- back/mongo.js
+++ back/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 const uniqueValidator = require("mongoose-unique-validator")
 
+
+
 const password = process.env.DB_PASSWORD
 const username = process.env.DB_USER
 
@@ -11,22 +13,8 @@ mongoose
   .then(() => console.log("Connected to Mongo!"))
   .catch((err) => console.error("Error connecting to Mongo: ", err))
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-})
-userSchema.plugin(uniqueValidator)
 
-const User = mongoose.model("User", userSchema)
 
 module.exports = {
-  mongoose,
-  User
+  mongoose
 }
